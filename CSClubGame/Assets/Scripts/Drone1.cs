37f1a61011714adbb86ba5a60a00ec29ce2dae6b
Tb64 <tabui@my.canyons.edu>
2016-11-02 23:22:24 -0700

    Adding 3D Models
    Addings tons of 3D models.

---
+++ CSClubGame/Assets/Scripts/Drone1.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drone1 : Enemy {
+
+    void Start()
+    {
+        ConfigEnemy("Drone", 1, true, 5, 7, 10, 25, 5, 5, 5, 5);
+        //enemySightRangeCollider = GetComponent<CircleCollider2D> ();
+        //enemySightRangeCollider.radius = newRadius;
+
+    }
+}
