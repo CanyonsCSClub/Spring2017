fbcd9cc1d8b0ab81c0c6c1b498722b3459e6351d
RealDealLea1 <Chris@Chriss-MacBook-Pro.local>
2017-03-02 14:19:27 -0800

    The Tank Tutorial Completed
    Asked the folks in CSC2017 to make a tank tutorial to get familiar with
    unity. This is for reference.

---
+++ Tanks Tutorial/Tanks Tutorial/Assets/Scripts/Camera/CameraControl.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float m_DampTime = 0.2f;        //approximate time to wait to move to deired location         
+    public float m_ScreenEdgeBuffer = 4f;		//buffer around the edge of the scree
+    public float m_MinSize = 6.5f;		//minimum "zoom" of the camera
+    [HideInInspector] public Transform[] m_Targets; 
+
+
+    private Camera m_Camera;                        
+    private float m_ZoomSpeed;                      
+    private Vector3 m_MoveVelocity;                 
+    private Vector3 m_DesiredPosition;              
+
+
+    private void Awake()
+    {
+        m_Camera = GetComponentInChildren<Camera>();
+    }
+
+	//tanks are moving in fixed update so this will have to as well
+    private void FixedUpdate()
+    {
+        Move();
+        Zoom();
+    }
+
+
+    private void Move()
+    {
+        FindAveragePosition();
+        transform.position = Vector3.SmoothDamp(transform.position, m_DesiredPosition, ref m_MoveVelocity, m_DampTime);
+		//smoothly move the camera between positions 
+		//the ref takes into account how fast it is currently moving does some movement and then reset the variable to how fast it is now moving
+    }
+
+
+    private void FindAveragePosition()
+    {
+        Vector3 averagePos = new Vector3();
+        int numTargets = 0;
+
+        for (int i = 0; i < m_Targets.Length; i++)//most likely will be 2 because there is two players in local mode...
+			//in multiplayer it will expand to accompany total players
+        {
+            if (!m_Targets[i].gameObject.activeSelf)
+                continue;
+
+            averagePos += m_Targets[i].position;
+            numTargets++;
+        }
+
+        if (numTargets > 0)
+            averagePos /= numTargets;
+
+        averagePos.y = transform.position.y;
+        m_DesiredPosition = averagePos;
+    }
+
+
+    private void Zoom()
+    {
+        float requiredSize = FindRequiredSize();
+        m_Camera.orthographicSize = Mathf.SmoothDamp(m_Camera.orthographicSize, requiredSize, ref m_ZoomSpeed, m_DampTime);
+		//zooms out/in over time
+    }
+
+
+    private float FindRequiredSize()
+    {
+        Vector3 desiredLocalPos = transform.InverseTransformPoint(m_DesiredPosition);
+
+        float size = 0f;
+
+        for (int i = 0; i < m_Targets.Length; i++)
+        {
+            if (!m_Targets[i].gameObject.activeSelf)
+                continue;
+
+            Vector3 targetLocalPos = transform.InverseTransformPoint(m_Targets[i].position);
+            Vector3 desiredPosToTarget = targetLocalPos - desiredLocalPos;
+
+			size = Mathf.Max (size, Mathf.Abs (desiredPosToTarget.y));
+            size = Mathf.Max (size, Mathf.Abs (desiredPosToTarget.x) / m_Camera.aspect);
+        }
+        
+        size += m_ScreenEdgeBuffer;
+
+        size = Mathf.Max(size, m_MinSize);
+
+        return size;
+    }
+
+
+    public void SetStartPositionAndSize()
+    {
+        FindAveragePosition();
+
+        transform.position = m_DesiredPosition;
+
+        m_Camera.orthographicSize = FindRequiredSize();
+    }
+}
---
+++ Tanks Tutorial/Tanks Tutorial/Assets/Scripts/Tank/TankShooting.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TankShooting : MonoBehaviour
+{
+    public int m_PlayerNumber = 1;       
+    public Rigidbody m_Shell;            
+    public Transform m_FireTransform;    
+    public Slider m_AimSlider;           
+    public AudioSource m_ShootingAudio;  
+    public AudioClip m_ChargingClip;     
+    public AudioClip m_FireClip;         
+    public float m_MinLaunchForce = 15f; 
+    public float m_MaxLaunchForce = 30f; 
+    public float m_MaxChargeTime = 0.75f;
+
+    
+    private string m_FireButton;         
+    private float m_CurrentLaunchForce;  
+    private float m_ChargeSpeed;         
+    private bool m_Fired;                
+
+
+    private void OnEnable()
+    {
+        m_CurrentLaunchForce = m_MinLaunchForce;
+        m_AimSlider.value = m_MinLaunchForce;
+    }
+
+
+    private void Start()
+    {
+        m_FireButton = "Fire" + m_PlayerNumber;
+
+        m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime;
+    }
+    
+
+    private void Update()
+    {
+        // Track the current state of the fire button and make decisions based on the current launch force.
+		m_AimSlider.value = m_MinLaunchForce;
+
+		if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired) {
+			//at max charge, not fired yet
+			m_CurrentLaunchForce = m_MaxLaunchForce;
+			Fire ();
+
+		} else if (Input.GetButtonDown (m_FireButton)) {
+			//have we pressed fire
+			m_Fired = false;
+			m_CurrentLaunchForce = m_MinLaunchForce;
+
+			m_ShootingAudio.clip = m_ChargingClip;
+			m_ShootingAudio.Play ();
+		
+		} else if (Input.GetButton (m_FireButton) && !m_Fired) {
+			//Holding Firebutton, not yet fired
+			m_CurrentLaunchForce += m_ChargeSpeed *Time.deltaTime;
+			m_AimSlider.value = m_CurrentLaunchForce;
+
+		} else if (Input.GetButtonUp (m_FireButton) && !m_Fired) {
+			//we released the button, not fired yet
+			Fire();
+
+		}
+    }
+
+    private void Fire()
+    {
+        // Instantiate and launch the shell.
+		m_Fired = true; 
+		Rigidbody shellInstance = Instantiate (m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+		//created the shell
+		shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward;
+		//launched the shell
+		m_ShootingAudio.clip = m_FireClip;
+		m_ShootingAudio.Play ();
+
+		m_CurrentLaunchForce = m_MinLaunchForce;
+    }
+}
