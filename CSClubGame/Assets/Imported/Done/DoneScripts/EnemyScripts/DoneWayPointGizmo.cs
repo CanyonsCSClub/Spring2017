1a546795e9ec0e5018b3d166894ac4875ab19d51
Tb64 <tabui@my.canyons.edu>
2017-04-21 23:29:27 -0700

    Removing Fall 2016 Project
    Fall 2016 Project Stored in "Fall2016" Branch
    https://github.com/RealDealLea1/canyonsCSclub/tree/Fall2016
    
    Folder found here:
    
    https://github.com/RealDealLea1/canyonsCSclub/tree/Fall2016/CSClubGameFA2016/Assets

--- CSClubGame/Assets/Imported/Done/DoneScripts/EnemyScripts/DoneWayPointGizmo.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public class DoneWayPointGizmo : MonoBehaviour {
-
-    void OnDrawGizmos() {
-        Gizmos.DrawIcon(transform.position, "wayPoint.png", true);
-    }
-}
