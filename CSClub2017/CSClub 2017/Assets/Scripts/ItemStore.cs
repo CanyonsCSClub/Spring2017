a9106f6cf5b72d33b2e2dc0234111f0815ba22d1
Persomatey <huntergoodin1@hotmail.com>
2017-05-12 00:23:35 -0700

    Item Storage - CL 003
    I implimented the if/else statement and gerardo made a bad comment
    header

--- CSClub2017/CSClub 2017/Assets/Scripts/ItemStore.cs
+++ CSClub2017/CSClub 2017/Assets/Scripts/ItemStore.cs
@@ -1,19 +1,30 @@
-﻿using System.Collections;
+﻿/* Programmer: Hunter Goodin
+
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemStore : MonoBehaviour
 {
-
     // Use this for initialization
     void Start()
     {
         string tagStr = "placeholder"; // this string will hold the tag of the object the user is currently holding
-
         // Write input stream that stores the object's tag in the string tagStr 
         // ^ NEI on the objects to do this 
 
-        GameObject[] nightStand = GameObject.FindGameObjectsWithTag(tagStr); // Stores the object the user is holding in the nightStand array 
+        GameObject[] nightStand; 
+        if (nightStand.length == 0) 
+        {
+            nightStand[0] = GameObject.FindGameObjectsWithTag(tagStr); // Stores the object the user is holding in the nightStand array 
+        }
+        else
+        {
+            playerArray[0] = GameObject.FindGameObjectsWithTag(tagStr); // Stores the object in the nightStand array to the playerArray (or whatever the player array is called) 
+        }
+        
     }
 	
 	// Update is called once per frame 
