3dc5367fe1673bff9bd5c9f3d897f52a3e638b92
Tb64 <Tb64@users.noreply.github.com>
2017-05-13 16:57:57 -0700

    Removed Code
    This code was breaking everything.

--- CSClub2017/CSClub 2017/Assets/Scripts/ItemStore.cs
+++ CSClub2017/CSClub 2017/Assets/Scripts/ItemStore.cs
@@ -15,15 +15,16 @@ public class ItemStore : MonoBehaviour
         // Write input stream that stores the object's tag in the string tagStr 
         // ^ NEI on the objects to do this 
 
-        GameObject[] nightStand; 
-        if (nightStand.length == 0) 
-        {
-            nightStand[0] = GameObject.FindGameObjectsWithTag(tagStr); // Stores the object the user is holding in the nightStand array 
-        }
-        else
-        {
-            playerArray[0] = GameObject.FindGameObjectsWithTag(tagStr); // Stores the object in the nightStand array to the playerArray (or whatever the player array is called) 
-        }
+	//Removed Code, it is broken. 
+        //GameObject[] nightStand; 
+        //if (nightStand.length == 0) 
+        //{
+        //    nightStand[0] = GameObject.FindGameObjectsWithTag(tagStr); // Stores the object the user is holding in the nightStand array 
+        //}
+        //else
+        //{
+        //    playerArray[0] = GameObject.FindGameObjectsWithTag(tagStr); // Stores the object in the nightStand array to the playerArray (or whatever the player array is called) 
+        //}
         
     }
 	
