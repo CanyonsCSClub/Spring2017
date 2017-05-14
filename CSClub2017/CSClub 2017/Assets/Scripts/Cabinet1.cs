741339c1f693885bccc57040618c67dc6aec55e7
Tb64 <tabui@my.canyons.edu>
2017-05-13 18:55:10 -0700

    Animated Cabinet Door
    Animated Cabinet Door

--- CSClub2017/CSClub 2017/Assets/Scripts/Cabinet1.cs
+++ CSClub2017/CSClub 2017/Assets/Scripts/Cabinet1.cs
@@ -8,38 +8,21 @@ public class Cabinet1 : MonoBehaviour {
     //public GameObject Collider;
     public GameObject Door;
     public string uiString;
-    private Transform pivot;
 
     private BoxCollider doorColider;
     private MeshRenderer doorRender;
     private Text textCanvas;
 
-    bool open;
-    float smooth = 2.0f;
-    float DoorOpenAngle = 90.0f;
-    float DoorCloseAngle = 0.0f;
-    Vector3 doorPivot;
+
 
     // Use this for initialization
     void Start () {
-        open = false;
         Debug.Log("trigger start");
 
         if(Door != null)
         {
             doorColider = Door.GetComponent<BoxCollider>();
             doorRender = Door.GetComponent<MeshRenderer>();
-            pivot = Door.GetComponentInChildren<Transform>();
-
-            if(pivot != null)
-            {
-                doorPivot = new Vector3(
-                    pivot.position.x,
-                    pivot.position.y,
-                    pivot.position.z);
-
-                Debug.Log("Pivot " + doorPivot);
-            }
         }
         
 
@@ -54,15 +37,7 @@ public class Cabinet1 : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if(open)
-        {
-            var target = Quaternion.Euler(0, transform.localRotation.eulerAngles.y + DoorOpenAngle, 0);
-            Quaternion doorRoation = Quaternion.Slerp(Door.transform.localRotation, target, Time.deltaTime * smooth);
-            float doorAngle = 1f;
-            //Vector3 doorPivot = new Vector3( pivot.transform.position.x , , );
-            Door.transform.RotateAround(doorPivot,Vector3.up, doorAngle);
 
-        }
     }
 
     /// <summary>
@@ -78,14 +53,9 @@ public class Cabinet1 : MonoBehaviour {
             if (Input.GetButtonDown("Use"))
             {
                 Debug.Log("Player Pressed Use.");
-                
-                //TEMP CODE
-                //Removing door instead of animating until animation is done.
-                doorColider.enabled = !doorColider.enabled;
-                doorRender.enabled = !doorRender.enabled;
-                //textCanvas.enabled = !textCanvas.enabled;
-                if (!Door.GetComponent<MeshRenderer>().enabled)
-                    other.GetComponent<PlayerControl>().HideDisable();
+
+                Door.GetComponent<OpenDoor>().AnimDoor();
+                other.GetComponent<PlayerControl>().HideDisable();
             }
         }
     }
@@ -97,7 +67,6 @@ public class Cabinet1 : MonoBehaviour {
         {
             Debug.Log("Setting text = " + uiString);
             other.GetComponent<PlayerControl>().UI_Set_Text(this.uiString);
-            open = true;
         }
     }
 
--- CSClub2017/CSClub 2017/Assets/Scripts/OpenDoor.cs
+++ CSClub2017/CSClub 2017/Assets/Scripts/OpenDoor.cs
@@ -4,55 +4,87 @@ using UnityEngine;
 
 public class OpenDoor : MonoBehaviour
 {
+    public GameObject pivotObj;
 
-    float smooth = 2.0f;
-    float DoorOpenAngle = 90.0f;
-    float DoorCloseAngle = 0.0f;
-    bool open;
-    bool enter;
+    bool open = false;
+    bool movingDoor = false;
+    float smooth = 3.0f;
+    float totalMovement = 0f;
+    float DoorOpenAngle = 45.0f;
+    float DoorCloseAngle;
+    Vector3 doorPivot;
+    float doorTime = 0f;
+    private Transform pivot;
 
-
-
-    public void Open()
+    private void Start()
     {
-        open = true;
+        DoorCloseAngle = transform.rotation.eulerAngles.y;
+        DoorOpenAngle += DoorCloseAngle;
+
+        if (pivotObj != null)
+            pivot = pivotObj.GetComponentInChildren<Transform>();
+        else
+            Debug.Log("Door Pivot Missing");
+
+        if (pivot != null)
+        {
+            doorPivot = new Vector3(
+                pivot.position.x,
+                pivot.position.y,
+                pivot.position.z);
+
+            //Debug.Log("Pivot " + doorPivot);
+        }
     }
-
-
-    public void Close()
-    {
-        open = false;
-
-        
-    }
-
-    
 
     void Update()
     {
-        if (open == true)
+        if (pivotObj != null && movingDoor)
         {
-            print("if(open)");
-            var target = Quaternion.Euler(0, DoorOpenAngle, 0);
-            transform.localRotation = Quaternion.Slerp(transform.localRotation, target, Time.deltaTime * smooth);
-        }
-
-        if (open == false)
-        {
-            print("if(!open)");
-            var target1 = Quaternion.Euler(0, DoorCloseAngle, 0);
-            transform.localRotation = Quaternion.Slerp(transform.localRotation, target1, Time.deltaTime * smooth);
-        }
-
-        if (enter == true)
-        {
-            print("if(enter)");
-
+            if (!open)
+            {
+                if (totalMovement >= 60f)
+                {
+                    open = true;
+                    movingDoor = false;
+                }
+                transform.RotateAround(doorPivot, Vector3.up, smooth);
+                totalMovement += smooth;
+            }
+            else
+            {
+                if (totalMovement <= 0)
+                {
+                    open = false;
+                    movingDoor = false;
+                }
+                transform.RotateAround(doorPivot, Vector3.up, 0 - smooth);
+                totalMovement -= smooth;
+            }
         }
     }
 
     public bool isOpen()
     {
-        return open;
+        return (open || movingDoor);
     }
+
+    public void Open()
+    {
+        if(!open)
+            movingDoor = true;
+    }
+
+
+    public void Close()
+    {
+        if(open)
+            movingDoor = true;
+    }
+
+    public void AnimDoor()
+    {
+        movingDoor = true;
+    }
+
 }
